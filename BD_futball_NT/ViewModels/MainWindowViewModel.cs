ff20f0deeffe459d6818a041caca8c1f3dd01875
Дмитрий Белобабов <stalker98belll@gmail.com>
2022-04-21 17:44:10 +0700

    Выполнен 2й этап курсовой работы

---
+++ BD_futball_NT/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BD_futball_NT.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
